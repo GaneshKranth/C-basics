97d8c6ea57be379d5d66d33734991fe6e99a31e8
GaneshKranth <92708317+GaneshKranth@users.noreply.github.com>
2021-10-20 11:00:18 +0530

    Add files via upload

---
+++ c basics.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+	//Variable Declaration
+	int age;
+	float cgpa;
+	char initial;
+	
+	//Initialization
+	age = 17;
+	cgpa = 8.6;
+	initial = 'B';
+	
+	//Output
+	printf("%d\n", age);
+	printf("%f\n", cgpa);
+	printf("%c", initial);
+	
+}
